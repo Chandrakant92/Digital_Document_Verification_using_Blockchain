6df994f07b2c8e08f120b3d2cc3f9b056e9f1c47
ykstech <admob5364@gmail.com>
2023-12-20 22:33:05 +0530

    login ui done

--- main/frontend/src/pages/LoginPage.jsx
+++ main/frontend/src/pages/LoginPage.jsx
@@ -1,162 +1,138 @@
 import React, { useState } from 'react';
-import ToggleSwitch from '../components/ToggleSwitch';
-import { Stepper, Step, Button, Select, Input, Option } from "@material-tailwind/react";
-import { Container } from 'postcss';
+import { Stepper, Step, Button, Select, Input, Option, IconButton } from "@material-tailwind/react";
+import { useForm, Controller } from 'react-hook-form';
+import { motion, AnimatePresence } from 'framer-motion';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 const LoginPage = () => {
-  const steps = [
-    { title: 'Select Role' },
-    { title: 'Enter Login Details' },
+
+
+  const { control, register, handleSubmit, formState: { errors }, watch } = useForm();
+  const selectedRole = watch('role');
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+
+  const stepVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+    exit: { opacity: 0, transition: { duration: 0 } },
+  };
+
+  const inputFields = [
+    { name: 'name', label: 'Username', type: 'text', required: true },
+    { name: 'email', label: 'Email', type: 'email', required: true },
+    { name: 'password', label: 'Password', type: 'password', required: true },
+    { name: 'address', label: 'Wallet Address', type: 'text', required: false },
+    { name: 'cid', label: 'Company Id', type: 'text', required: true },
+    { name: 'uid', label: 'University Id', type: 'text', required: true },
   ];
 
+  const filteredFields = inputFields.filter(
+    field => (field.name !== 'cid' && field.name !== 'uid') || (field.name === 'cid' && selectedRole === 'company') || (field.name === 'uid' && selectedRole === 'university')
+  );
+
   const [activeStep, setActiveStep] = useState(0);
-  const [role, setRole] = useState('');
-  const [loginInfo, setLoginInfo] = useState({
-    uid: '', // Extra field for the university
-    name: '',
-    email: '',
-    password: '',
-    address: '',
-    cid: '', // Extra field for the company
-  });
-
-  const handleRoleSelection = (selectedRole) => {
-    setRole(selectedRole);
-    if (selectedRole === '') return;
-   
-    setActiveStep(1);
-    setLoginInfo({
-      uid: '',
-      name: '',
-      email: '',
-      password: '',
-      address: '',
-      cid: '',
-    });
-  };
-
-  const handleLoginInfoChange = (e) => {
-    const { name, value } = e.target;
-    setLoginInfo({ ...loginInfo, [name]: value });
-  };
-
-  const handleSubmit = () => {
-    // Handle submission logic here
-    console.log('Submitted:', loginInfo);
-    // Reset form after submission
-    setRole('');
-    setLoginInfo({
-      uid: '',
-      name: '',
-      email: '',
-      password: '',
-      address: '',
-      cid: '',
-    });
-  };
 
   const [isLastStep, setIsLastStep] = React.useState(false);
   const [isFirstStep, setIsFirstStep] = React.useState(false);
 
-  const handleNext = () => !isLastStep && role != '' &&setActiveStep((cur) => cur + 1);
+  const handleNext = () => !isLastStep && selectedRole && setActiveStep((cur) => cur + 1);
   const handlePrev = () => !isFirstStep && setActiveStep((cur) => cur - 1);
 
 
   return (
 
-    <div className="flex items-center justify-center m-5">
-      <div className="w-96  py-4 px-8">
-        <Stepper
-          activeStep={activeStep}
-          isLastStep={(value) => setIsLastStep(value)}
-          isFirstStep={(value) => setIsFirstStep(value)}
-        >
-          <Step onClick={() => setActiveStep(0)}>1</Step>
-          <Step onClick={() => role!='' && setActiveStep(1)}>2</Step>
-        </Stepper>
+    //<div className="flex items-center justify-center m-5">
+    <div className="flex justify-center h-screen">
+      <div className="relative w-1/2  flex flex-col items-center  ">
+        <div className="absolute left-0 top-1/3 transform -translate-y-1/2">
+          <IconButton onClick={handlePrev} disabled={isFirstStep} className="rounded-full" >
+            <ChevronLeftIcon className="h-6 w-6 text-white" />
+          </IconButton>
+        </div>
+        <div className="absolute right-0 top-1/3 transform -translate-y-1/2">
+          <IconButton onClick={handleNext} disabled={isLastStep || !selectedRole} className="rounded-full" >
+            <ChevronRightIcon className="h-6 w-6 text-white" />
+          </IconButton>
+        </div>
+
+        <div className="mt-10 w-96  ">
+          <Stepper
+            activeStep={activeStep}
+            isLastStep={(value) => setIsLastStep(value)}
+            isFirstStep={(value) => setIsFirstStep(value)}
+          >
+            <Step onClick={() => setActiveStep(0)}>1</Step>
+            <Step onClick={() => selectedRole && setActiveStep(1)}>2</Step>
+          </Stepper>
 
 
 
-        {activeStep === 0 && (
-          <div className="mt-5">
-            <Select label="Select Role"
-             value={role}
-              onChange={(e) => handleRoleSelection(e)}
-            >
-              <Option value="">Select User Role</Option>
-              <Option value="student">Student</Option>
-              <Option value="university">University</Option>
-              <Option value="company">Company</Option>
-            </Select>
-          </div>
-        )}
-        {activeStep === 1 && role != '' && (
-          <div className="mt-5 flex flex-col space-y-4">
-            <Input label="Username"
-              type="text"
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <AnimatePresence mode="wait">
 
-              value={loginInfo.name}
-              onChange={handleLoginInfoChange}
-            />
-            <Input label="Email"
-              type="email"
+              {activeStep === 0 && (
+                <motion.div
+                  key={1}
+                  variants={stepVariants}
+                  initial="visible"
+                  animate="visible"
+                  exit="exit"
+                >
+                  <div className="mt-20 items-center">
+                    <Controller
+                      name="role"
+                      control={control}
+                      defaultValue=""
+                      rules={{ required: true }}
+                      render={({ field }) => (
+                        <Select label="Select Role" {...field}>
+                          <Option value="student">Student</Option>
+                          <Option value="university">University</Option>
+                          <Option value="company">Company</Option>
+                        </Select>
+                      )}
+                    />
+                  </div>
+                </motion.div>
+              )}
 
-              value={loginInfo.email}
-              onChange={handleLoginInfoChange}
-            />
+              {activeStep === 1 && selectedRole && (
+                <motion.div
+                  key={2}
+                  variants={stepVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                >
+                  <div className="mt-20 flex flex-col space-y-4">
+                    {filteredFields.map((field, index) => (
+                      <Input
+                        key={index}
+                        label={field.label}
+                        type={field.type}
+                        {...register(field.name, { required: field.required })}
+                      />
+                    ))}
 
-            {role === 'company' && (
-              <Input label="Company Id"
-                type="text"
+                    <Button type="submit" ripple={true}>
+                      Submit
+                    </Button>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
 
-                value={loginInfo.cid}
-                onChange={handleLoginInfoChange}
-              />
-
-            )}
-            {role === 'university' && (
-              <Input label="University Id"
-                type="text"
-
-                value={loginInfo.uid}
-                onChange={handleLoginInfoChange}
-              />
-
-            )}
-            <Input label="password"
-              type="password"
-
-              value={loginInfo.password}
-              onChange={handleLoginInfoChange}
-            />
-            <Input label="Wallet Address"
-              type="text"
-
-              value={loginInfo.address}
-              onChange={handleLoginInfoChange}
-            />
-
-            <Button onClick={handleSubmit}
-              disabled={!loginInfo.email || !loginInfo.password}
-              ripple={true}>
-              Submit
-            </Button>
-          </div>
-        )}
+          </form>
 
 
 
-        <div className="mt-16 flex justify-between ">
-          <Button onClick={handlePrev} disabled={isFirstStep}>
-            Prev
-          </Button>
-          <Button onClick={handleNext} disabled={isLastStep || role==''}>
-            Next
-          </Button>
         </div>
       </div>
     </div>
-
 
 
   );

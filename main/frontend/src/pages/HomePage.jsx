a656ad7facbb6cc23cf51798f069bb5115e0d654
ykstech <admob5364@gmail.com>
2024-01-20 18:32:09 +0530

    updated

--- main/frontend/src/pages/HomePage.jsx
+++ main/frontend/src/pages/HomePage.jsx
@@ -21,22 +21,9 @@ import { FaBuilding } from "react-icons/fa";
 import IconBox from "../components/icons/IconBox";
 import RoleInfoBox from "../mainComponents/RoleInfoBox";
 import ThreeDModelCanvas from "../mainComponents/ThreeDModelCanvas";
+import UniversalModel from "../mainComponents/UniversalModel";
 
 
-
-const UniversalModel = ({ modelname, position, rotation, scale }) => {
-  const gltf = useLoader(GLTFLoader, `./model/${modelname}/scene.gltf`);
-  gltf.scene.position.set(position.x, position.y, position.z);
-  gltf.scene.rotation.x = rotation.x;
-  gltf.scene.rotation.y = rotation.y;
-  gltf.scene.rotation.z = rotation.z;
-
-  return (
-    <>
-      <primitive object={gltf.scene} scale={scale} />
-    </>
-  );
-};
 
 
 export default function HomePage() {

7f614735bdbfda25ed9d55f0cffb6b607284fc90
ykstech <admob5364@gmail.com>
2024-01-23 05:25:25 +0530

    all done

--- main/frontend/src/pages/Test.jsx
+++ main/frontend/src/pages/Test.jsx
@@ -4,61 +4,90 @@ import { Box, Button, Flex, Image, Input, Stack, Text } from '@chakra-ui/react';
 
 const Test = () => {
   const [name, setName] = useState('Yash Sugandhi');
+  const [about, setAbout] = useState('has demonstrated exceptional commitment and excellence, earning this certificate as a testament to their commendable accomplishments in');
+
+  const [event, setEvent] = useState('Tabel Tennis');
+
+  
   const [organization, setOrganization] = useState('SSGMCE');
-  const [signer, setSigner] = useState("Principal");
   const certificateRef = useRef(null);
 
- 
+
 
   const handleExport = () => {
     exportComponentAsPNG(certificateRef, { fileName: 'certificate' });
   };
 
   return (
-    <Flex justifyContent={'space-evenly'}>
+    <Flex >
 
 
       {/* User Input Fields */}
-      <Box  p='5' bg='white' borderRadius='20px' >
+      <Box p='5' bg='white' borderRadius='20px' >
         <Stack spacing={'5'}>
-        <label>Name:</label>
-        <Input variant={'auth'}
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value )}
-         />
-    
-        <label>Organization:</label>
-        <Input variant={'auth'}
-          type="text"
-          value={organization}
-          onChange={(e) => setOrganization( e.target.value )}
-         />
+          <label>Name:</label>
+          <Input variant={'auth'}
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+           <label>About:</label>
+          <Input variant={'auth'}
+            type="text"
+            value={about} 
+            onChange={(e) => setAbout(e.target.value)}
+          />
+          <label>Event:</label>
+          <Input variant={'auth'}
+            type="text"
+            value={event}
+            onChange={(e) => setEvent(e.target.value)}
+          />
 
-        <label>Signer:</label>
-        <Input variant={'auth'}
-          type="text"
-          value={signer}
-          onChange={(e) => setSigner(e.target.value )}
-        />
-        <Button onClick={handleExport}>Download Certificate</Button>
+          <label>Organization:</label>
+          <Input variant={'auth'}
+            type="text"
+            value={organization}
+            onChange={(e) => setOrganization(e.target.value)}
+          />
+
+
+          <Button onClick={handleExport}>Download Certificate</Button>
         </Stack>
       </Box>
 
 
       {/* Certificate */}
-      <div ref={certificateRef} bg='white' >
-        {/* Text Boxes */}
-        <img
-   
-    src='./images/c1.png'
-    alt='Dan Abramov'
-  
-    width='600px'
-  />
-      </div>
-
+      <div ref={certificateRef} style={{ position: 'relative' ,display: "inline-block"}}>
       
+    <img 
+      src='./images/c1.png'
+      alt='Certificate Background'
+      width='800px' height="700px"
+      style={{display: "block"}}
+    />
+ 
+  <Box  position={"absolute"}  top={230} left={0} m="100">
+    <Text align="center" fontSize='22px' fontWeight='400' lineHeight='100%' m="2">
+      {name}
+    </Text>
+    <Text align="center" fontSize='16px' fontWeight='400' lineHeight='100%' m="3">
+   <br/>
+    {about}
+    </Text>
+    <Text align="center"  fontSize='16px' fontWeight='600' lineHeight='100%' m="2">
+      {event}
+    </Text>
+    <Text align="center" fontSize='22px' fontWeight='400' lineHeight='100%' m="2">
+      <br/><br/><br/>
+      {organization}
+    </Text>
+  </Box>
+  
+</div>
+
+
+
     </Flex>
   );
 };
